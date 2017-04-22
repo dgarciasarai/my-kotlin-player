c16e481c64ed4dd8617e7d6762dcc30d48e2e9c3
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 11:27:26 +0200

    Add fetchMedia function to take random pictures from LoremPixel
    Add internet permission too

---
+++ app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
@@ -0,0 +1,17 @@
+package com.dgarciasarai.myplayer
+
+/**
+ * @author Sarai Díaz García
+ */
+fun fetchMedia(): List<Item> = listOf(
+    Item("Title 1", "http://lorempixel.com/400/400/cats/1"),
+    Item("Title 2", "http://lorempixel.com/400/400/cats/2"),
+    Item("Title 3", "http://lorempixel.com/400/400/cats/3"),
+    Item("Title 4", "http://lorempixel.com/400/400/cats/4"),
+    Item("Title 5", "http://lorempixel.com/400/400/cats/5"),
+    Item("Title 6", "http://lorempixel.com/400/400/cats/6"),
+    Item("Title 7", "http://lorempixel.com/400/400/cats/7"),
+    Item("Title 8", "http://lorempixel.com/400/400/cats/8"),
+    Item("Title 9", "http://lorempixel.com/400/400/cats/9"),
+    Item("Title 10", "http://lorempixel.com/400/400/cats/10")
+)

8aa95ddc1317d5a2e59c40e0ff907bcbd69df561
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 17:58:49 +0200

    Working with null (?)

--- app/src/main/java/com/dgarciasarai/myplayer/Test.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/Test.kt
@@ -7,17 +7,7 @@ import android.view.View
  */
 fun test(view: View) {
 
-    val list = listOf(1, 2, 3, 4)
-
-    for (item in list) {
-        //Iterate and do whatever
-    }
-
-    for (i in 1..10) {
-        //Iterate from 1 to 10
-    }
-
-    for (i in 1 until 10) {
-        //Iterate from 1 to 9
-    }
+    val item: Item? = null
+    item.title //Null
+    item?.title //Good!
 }

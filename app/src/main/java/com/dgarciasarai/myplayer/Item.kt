eabf76190d4be190abb4d6493662d49d77017cf5
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 12:59:39 +0200

    Add type to Item

--- app/src/main/java/com/dgarciasarai/myplayer/Item.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/Item.kt
@@ -3,4 +3,6 @@ package com.dgarciasarai.myplayer
 /**
  * @author Sarai Díaz García
  */
-data class Item(val title: String, val url: String)
+data class Item(val title: String, val url: String, val type: Type) {
+    enum class Type { PHOTO, VIDEO }
+}

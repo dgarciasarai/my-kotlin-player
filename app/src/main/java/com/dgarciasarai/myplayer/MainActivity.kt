706dbe16724fadcc75db66ccd08f1dd77b217bb5
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 16:38:56 +0200

    Create apply2 extension funtion

--- app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dgarciasarai.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         recycler.adapter = MediaAdapter(fetchMedia()) {
             toast(it.title)
         }
+
+        TextView(this).apply2 {
+            textSize = 20f
+        }
     }
+}
+
+fun <T : Any> T.apply2(f: T.() -> Unit): T {
+    this.f()
+    return this
 }

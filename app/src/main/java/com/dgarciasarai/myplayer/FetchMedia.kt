7345ea4b2ef1117229079aab3ec6c395e69a39cd
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 17:34:22 +0200

    Change range 1 to 10 (we don't want Title 0 :P)

--- app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
@@ -4,7 +4,7 @@ package com.dgarciasarai.myplayer
  * @author Sarai Díaz García
  */
 fun fetchMedia() =
-    (0..10).map {
+    (1..10).map {
         Item("Title $it", "http://lorempixel.com/400/400/cats/$it",
             if (it % 3 == 0) Item.Type.VIDEO else Item.Type.PHOTO)
     }

8cfe45bd0db5b0bf81684ef4f4294a5070422a41
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 13:39:19 +0200

    Use Kotlin Android Entensions to get the itemView elements

--- app/src/main/java/com/dgarciasarai/myplayer/MediaAdapter.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MediaAdapter.kt
@@ -3,8 +3,7 @@ package com.dgarciasarai.myplayer
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import kotlinx.android.synthetic.main.view_media_item.view.*
 
 /**
  * @author Sarai Díaz García
@@ -23,14 +22,10 @@ class MediaAdapter(val data: List<Item>) : RecyclerView.Adapter<MediaAdapter.Med
 
     class MediaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val title = find<TextView>(R.id.media_title)
-        val thumb = find<ImageView>(R.id.media_thumb)
-        val mediaVideoIndicator = find<ImageView>(R.id.media_video_indicator)
-
         fun bind(item: Item) {
-            title.text = item.title
-            thumb.loadUrl(item.url)
-            mediaVideoIndicator.visible = item.type == Item.Type.VIDEO
+            itemView.media_title.text = item.title
+            itemView.media_thumb.loadUrl(item.url)
+            itemView.media_video_indicator.visible = item.type == Item.Type.VIDEO
             itemView.setOnClickListener { toast(item.title) }
         }
     }

68634f66569eb058a1cbd5f83aae0e4363e14944
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 17:02:57 +0200

    Use by lazy to declare recycler

--- app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
@@ -2,11 +2,16 @@ package com.dgarciasarai.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val recycler by lazy { find<RecyclerView>(R.id.recycler) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler.adapter = MediaAdapter(fetchMedia()) { toast(it.title) }
     }
 }

c1a0543cfa479fc69c97abba85be27d0f152a276
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 10:25:23 +0200

    Add val to name property and now we can access it

--- app/src/main/java/com/dgarciasarai/myplayer/Person.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/Person.kt
@@ -3,11 +3,11 @@ package com.dgarciasarai.myplayer
 /**
  * @author Sarai Díaz García
  */
-open class Person(name: String, val age: Int)
+open class Person(val name: String, val age: Int)
 
 class Developer(name: String) : Person(name, 24)
 
 fun test() {
     val person = Person("Name", 20)
-    person.age
+    person.name
 }

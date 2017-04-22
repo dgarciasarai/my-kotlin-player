58a58fa672dd44d6fe93cc6f6bb9a0728ee6cac7
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 11:17:54 +0200

    Create Adapter and ViewHolder for our RecyclerView

---
+++ app/src/main/java/com/dgarciasarai/myplayer/MediaAdapter.kt
@@ -0,0 +1,35 @@
+package com.dgarciasarai.myplayer
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+/**
+ * @author Sarai Díaz García
+ */
+class MediaAdapter(val data: List<Item>) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_media_item, parent, false)
+        return MediaViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount() = data.size
+
+    class MediaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val title = itemView.findViewById(R.id.media_title) as TextView
+        val thumb = itemView.findViewById(R.id.media_thumb) as ImageView
+
+        fun bind(item: Item) {
+            title.text = item.title
+        }
+    }
+}

c532d91361b8fe7f4ae6e6119d0d6aa0df0fd947
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 13:35:42 +0200

    Use Kotlin Android Extensions to get the RecyclerView

--- app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
@@ -2,16 +2,14 @@ package com.dgarciasarai.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //val recyclerView = find<RecyclerView>(R.id.recycler)
-        val recyclerView = findViewById(R.id.recycler) as RecyclerView
-        recyclerView.adapter = MediaAdapter(fetchMedia())
+        
+        recycler.adapter = MediaAdapter(fetchMedia())
     }
 }

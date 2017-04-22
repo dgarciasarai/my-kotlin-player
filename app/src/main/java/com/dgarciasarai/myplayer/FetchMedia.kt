9589cb30f13c6fb6cf9d06122a89d795ec897638
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 17:25:12 +0200

    Use new fetchMedia to retrieve information

--- app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
@@ -3,20 +3,7 @@ package com.dgarciasarai.myplayer
 /**
  * @author Sarai Díaz García
  */
-fun fetchMedia(): List<Item> = listOf(
-    Item("Title 1", "http://lorempixel.com/400/400/cats/1", Item.Type.PHOTO),
-    Item("Title 2", "http://lorempixel.com/400/400/cats/2", Item.Type.VIDEO),
-    Item("Title 3", "http://lorempixel.com/400/400/cats/3", Item.Type.PHOTO),
-    Item("Title 4", "http://lorempixel.com/400/400/cats/4", Item.Type.PHOTO),
-    Item("Title 5", "http://lorempixel.com/400/400/cats/5", Item.Type.VIDEO),
-    Item("Title 6", "http://lorempixel.com/400/400/cats/6", Item.Type.VIDEO),
-    Item("Title 7", "http://lorempixel.com/400/400/cats/7", Item.Type.PHOTO),
-    Item("Title 8", "http://lorempixel.com/400/400/cats/8", Item.Type.PHOTO),
-    Item("Title 9", "http://lorempixel.com/400/400/cats/9", Item.Type.VIDEO),
-    Item("Title 10", "http://lorempixel.com/400/400/cats/10", Item.Type.PHOTO)
-)
-
-fun fetchMedia2() =
+fun fetchMedia() =
     (0..10).map {
         Item("Title $it", "http://lorempixel.com/400/400/cats/$it",
             if (it % 3 == 0) Item.Type.VIDEO else Item.Type.PHOTO)

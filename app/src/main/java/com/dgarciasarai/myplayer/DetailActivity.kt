0b3e8b3ffac2245a585bb70f7900e71ca1ce933a
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 18:14:19 +0200

    Another way with let

--- app/src/main/java/com/dgarciasarai/myplayer/DetailActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/DetailActivity.kt
@@ -18,7 +18,7 @@ class DetailActivity : AppCompatActivity() {
 
         val item = MediaProvider.fetchMedia().firstOrNull() { it.id == id }
 
-        if (item != null) {
+        item?.let {
             supportActionBar?.title = item.title
             detail_thumb.loadUrl(item.url)
             detail_video_indicator.visible = item.type == Item.Type.VIDEO

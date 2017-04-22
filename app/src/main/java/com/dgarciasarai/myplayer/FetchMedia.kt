8bac327092e13ab3e68404adeabeaad5cb4d3fa5
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 17:38:37 +0200

    Create MediaProvider singleton for fetch media with by lazy

--- app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/FetchMedia.kt
@@ -3,8 +3,14 @@ package com.dgarciasarai.myplayer
 /**
  * @author Sarai Díaz García
  */
-fun fetchMedia() =
-    (1..10).map {
-        Item("Title $it", "http://lorempixel.com/400/400/cats/$it",
-            if (it % 3 == 0) Item.Type.VIDEO else Item.Type.PHOTO)
-    }
+
+object MediaProvider {
+
+    val data by lazy { fetchMedia() }
+
+    private fun fetchMedia() =
+        (1..10).map {
+            Item("Title $it", "http://lorempixel.com/400/400/cats/$it",
+                if (it % 3 == 0) Item.Type.VIDEO else Item.Type.PHOTO)
+        }
+}

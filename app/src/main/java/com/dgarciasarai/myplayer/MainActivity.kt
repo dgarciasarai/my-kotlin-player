27b5724c02436999eb2cc656e6f1939cbca0b088
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 09:49:31 +0200

    Retrieve the TextView and set another text value

--- app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
@@ -2,11 +2,15 @@ package com.dgarciasarai.myplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById(R.id.text_view) as TextView
+        textView.setText("Hello Kotlin!")
     }
 }

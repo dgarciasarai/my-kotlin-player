83e675481d71685e0f5c1ef6607c7dabe5782565
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 10:25:40 +0200

    Create MyClass with two constructors

--- app/src/main/java/com/dgarciasarai/myplayer/Person.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/Person.kt
@@ -7,6 +7,21 @@ open class Person(val name: String, val age: Int)
 
 class Developer(name: String) : Person(name, 24)
 
+class MyClass {
+    val name: String
+    val age: Int
+
+    constructor(name: String) {
+        this.name = name
+        this.age = 20
+    }
+
+    constructor(age: Int) {
+        this.name = ""
+        this.age = age
+    }
+}
+
 fun test() {
     val person = Person("Name", 20)
     person.name

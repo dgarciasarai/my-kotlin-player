1885783ebca8fd2f0c643badcc57574e851f353b
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-22 12:12:05 +0200

    Try toast extension

--- app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/myplayer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dgarciasarai.myplayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById(R.id.recycler) as RecyclerView
         recyclerView.adapter = MediaAdapter(fetchMedia())
 
+        toast("Hello Kotlin!")
     }
 }
 
